2c3f2c301274a211552bbd400c4a9cf0e351e40f
gr99murad <golamrabbanimurad@gmail.com>
2025-02-21 11:05:24 +0600

    Fetched all data by category

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Component/Navbar';
+import axios from 'axios';
 
 const Dashboard = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetchTasks();
+    },[]);
+    const fetchTasks = async () => {
+        const res = await axios.get('http://localhost:5000/tasks');
+        setTasks(res.data);
+    }
+    
     return (
         <>
         <Navbar></Navbar>
         <div>
-            dashboard
+            
         </div>
 
             
